9a916045ecc8095107c8cc7761113afeda9946db
Hyun-Tae Jin <htjmario@gmail.com>
2023-03-26 02:14:13 -0400

    condensed determineRank(), split code blocks

--- rps.py
+++ rps.py
@@ -16,40 +16,23 @@ def choosePlayer(player_name, playersDict, score = 0, combo = 0, max_combo = 0):
 
 def determineRank(score):
     rank = 'Newbie'
-    if score == 0:
-        return rank
-    elif 0 < score <= 5:
-        rank = 'Bronze'
-    elif 5 < score <= 10:
-        rank = 'Silver'
-    elif 10 < score <= 15:
-        rank = 'Gold'
-    elif 15 < score <= 20:
-        rank = 'Platinum'
-    elif 20 < score <= 25:
-        rank = 'Diamond'
-    elif 25 < score <= 30:
-        rank = 'Master'
-    elif 30 < score <= 35:
-        rank = 'Grandmaster'
-    elif score > 35:
-        rank = 'G O A T'
-    elif 0 > score >= -5:
-        rank = 'Egg'
-    elif -5 > score >= -10:
-        rank = 'Unlucky'
-    elif -10 > score >= -15:
-        rank = 'Loser'
-    elif -15 > score >= -20:
-        rank = 'Weenie'
-    elif -20 > score >= -25:
-        rank = 'Super Weenie'
-    elif -25 > score >= -30:
-        rank = 'Mega Weenie Loser'
-    elif -30 > score >= -35:
-        rank = 'Hardstuck Loser'
-    elif score < -35:
-        rank = 'Trash'
+    if score == 0: return rank
+    elif 0 < score <= 5: rank = 'Bronze'
+    elif 5 < score <= 10: rank = 'Silver'
+    elif 10 < score <= 15: rank = 'Gold'
+    elif 15 < score <= 20: rank = 'Platinum'
+    elif 20 < score <= 25: rank = 'Diamond'
+    elif 25 < score <= 30: rank = 'Master'
+    elif 30 < score <= 35: rank = 'Grandmaster'
+    elif score > 35: rank = 'G O A T'
+    elif 0 > score >= -5: rank = 'Egg'
+    elif -5 > score >= -10: rank = 'Unlucky'
+    elif -10 > score >= -15: rank = 'Loser'
+    elif -15 > score >= -20: rank = 'Weenie'
+    elif -20 > score >= -25: rank = 'Super Weenie'
+    elif -25 > score >= -30: rank = 'Mega Weenie Loser'
+    elif -30 > score >= -35: rank = 'Hardstuck Loser'
+    elif score < -35: rank = 'Trash'
     return rank
         
 
@@ -61,16 +44,21 @@ def playGame():
     score = 0
     combo = 0
     max_combo = 0
+    
     print(f'\nWelcome Player {user_name}!')
     rank = determineRank(0)
     print(f'Looks like your rank is: {rank}, with a score of {score}')
+    
     while True:
         cpu_move = generatePlay()
         player_move = input("\nThrow your move (Rock/Paper/Scissors or r/p/s), 'rank/score', 'create', 'switch', or type 'I quit': ").strip().lower()
+        
         if player_move not in moves:
             print('Player input not recognized, please try again.')
+        
         elif player_move == 'players':
             print(players)
+        
         elif player_move == 'create':
             players = choosePlayer(user_name, players, score, combo, max_combo)
             print('You chose to create a new player\n')
@@ -78,6 +66,7 @@ def playGame():
             print(f'\nWelcome Player {user_name}!')
             print(f'Looks like your rank is: Newbie, with a score of {0}')
             players = choosePlayer(user_name, players, 0, 0, 0)
+        
         elif player_move == 'switch':
             while True:
               if len(players) > 1:
@@ -96,9 +85,11 @@ def playGame():
                   rank = determineRank(players[user_name]['score'])
                   print(f"\n{user_name}'s rank is: {rank}, with a score of {players[user_name]['score']}")
                   break
+        
         elif player_move == 'rank' or player_move == 'score':
             rank = determineRank(players[user_name]['score'])
             print(f"\n{user_name}'s rank is: {rank}, with a score of {players[user_name]['score']}")
+        
         elif player_move == 'i quit':
             print('Thank you for playing')
             print('[ -- DAILY HIGHSCORES -- ]')
@@ -107,6 +98,7 @@ def playGame():
                 print(f"{place}. {player} ---------- Score: {item['score']}, Max Combo: {item['max_combo']} Rank: {determineRank(item['score'])}")
                 place += 1
             break
+        
         elif player_move == 'rock' or player_move == 'r':
             print(f'The computer played: {cpu_move}')
             if cpu_move == 'Rock':
@@ -124,6 +116,7 @@ def playGame():
                 score += 5
                 players = choosePlayer(user_name, players, score+combo, combo, max_combo)
                 print('You Win')
+        
         elif player_move == 'paper' or player_move == 'p':
             print(f'The computer played: {cpu_move}')
             if cpu_move == 'Rock':
@@ -141,6 +134,7 @@ def playGame():
                 score -= 5
                 players = choosePlayer(user_name, players, score, combo, max_combo)
                 print('You Lose')
+        
         elif player_move == 'scissors' or player_move == 's':
             print(f'The computer played: {cpu_move}')
             if cpu_move == 'Rock':
